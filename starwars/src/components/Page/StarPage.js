e18882ebb71146251a18f2eee726f0cae80a6448
Joshua <joshaw66@gmail.com>
2020-03-06 18:57:07 -0700

    finally running, time to style and change a few attributes

--- starwars/src/components/Page/StarPage.js
+++ starwars/src/components/Page/StarPage.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import styled from "styled-components";
 
-const Body = styled.div
-
-const Card = styled.div
 
 export function StarPage(props) {
+if (!props.person) return <h2>Here You Go!</h2>;
+
   return (
-    <Body>
-      <Card>
-        <h1>{props.data.name}</h1>
-        <h3>{props.data.eye_color}</h3>
-        <h3>{props.data.home_world}</h3>
-        <h3>{props.data.species}</h3>
-        <h3>{props.data.vehicles}</h3>
-      </Card>
-    </Body>
+    <div>
+      <div>
+        <h1>{props.person.name}</h1>
+        <h3>{props.person.eye_color}</h3>
+        <h3>{props.person.home_world}</h3>
+        <h3>{props.person.species}</h3>
+        <h3>{props.person.vehicles}</h3>
+      </div>
+    </div>
   );
-}
+  }
 
 export default StarPage;
