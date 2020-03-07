7a69db3b1b72b2b5cfa4cfaa795bff1a90d49c62
Joshua <joshaw66@gmail.com>
2020-03-06 17:20:22 -0700

    Started axios call on API

--- starwars/src/components/Data/Data.js
+++ starwars/src/components/Data/Data.js
@@ -1,3 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
+
+const Data = () => {
+  const [data, setData] = useState []);
+
+  useEffect(() => {
+    axios.get()
+  })
+}
 
