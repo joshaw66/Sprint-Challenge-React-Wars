5c7c75fb536797b078b7424f988a3154938fcc59
Joshua <joshaw66@gmail.com>
2020-03-06 17:35:34 -0700

    Finished Page.js and started imports

--- starwars/src/components/Page/Page.js
+++ starwars/src/components/Page/Page.js
@@ -1,3 +1,18 @@
 import React from 'react';
-import './App.css';
+import styled from "styled-components";
 
+const Page = props => {
+  return (
+    <body>
+      <information>
+        <h1>{props.data.name}</h1>
+        <h3>{props.data.eye_color}</h3>
+        <h3>{props.data.home_world}</h3>
+        <h3>{props.data.species}</h3>
+        <h3>{props.data.vehicles}</h3>
+      </information>
+    </body>
+  );
+}
+
+export default Page;
