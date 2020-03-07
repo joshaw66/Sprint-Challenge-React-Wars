1383ba596eb78fe049b2c7451b5c44efa11d8654
Joshua <joshaw66@gmail.com>
2020-03-06 19:12:42 -0700

    MVP met, styling needs to be polished!

--- starwars/src/components/Page/StarPage.js
+++ starwars/src/components/Page/StarPage.js
@@ -2,19 +2,39 @@ import React from 'react';
 import styled from "styled-components";
 
 
+// Header Styles //
+
+const StyledHeader1 = styled.h2`
+  font-size: 1.4 rem;
+  max-width: 100%;
+  color: white;
+
+  :hover {
+    color: aqua;
+  }
+  `;
+
+  // Card Styles //
+
+  const StyledCard = styled.p`
+    display: inline-block;
+    background: lightblue;
+    height: auto;
+    margin-bottom: 15px;
+    border: 3px solid black;
+    `;
+
 export function StarPage(props) {
 if (!props.person) return <h2>Here You Go!</h2>;
 
   return (
-    <div>
       <div>
-        <h1>{props.person.name}</h1>
-        <h3>{props.person.eye_color}</h3>
-        <h3>{props.person.home_world}</h3>
-        <h3>{props.person.species}</h3>
-        <h3>{props.person.vehicles}</h3>
+        <StyledHeader1>{props.person.name}</StyledHeader1>
+        <StyledCard>Eye Color: {props.person.eye_color}</StyledCard>
+        <StyledCard>Height: {props.person.height}</StyledCard>
+        <StyledCard>Mass: {props.person.mass}</StyledCard>
+        <StyledCard>Hair Color: {props.person.hair_color}</StyledCard>
       </div>
-    </div>
   );
   }
 
