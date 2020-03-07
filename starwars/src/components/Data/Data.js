f865046da106fb5f047791b26b363971a64d56dd
Joshua <joshaw66@gmail.com>
2020-03-06 17:25:48 -0700

    finished Data.js and axios call

--- starwars/src/components/Data/Data.js
+++ starwars/src/components/Data/Data.js
@@ -6,7 +6,24 @@ const Data = () => {
   const [data, setData] = useState []);
 
   useEffect(() => {
-    axios.get()
-  })
+    axios.get("http https://swapi.co/api/people/")
+    .then(response => {
+      console.log(response)
+      setData(response.data.results)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },[])
+  
+  return (
+    <div>
+      {data.map(i => (
+        <cardCreator data = {i}/>
+      ))}
+    </div>
+  );
 }
+
+export default Data;
 
